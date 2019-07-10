7dcbd37e5909c8b8827659c63094c07c52038807
Sven <1326860703@qq.com>
2019-07-10 10:06:20 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      msg: 'welcome to vue app'
+    }
+  }
+}
+</script>
